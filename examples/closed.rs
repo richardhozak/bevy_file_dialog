37db15c8e583481da3865bd9ec34275a382c8783
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-01-12 12:23:40 +0100

    Make 'closed' more interactive

--- examples/closed.rs
+++ examples/closed.rs
@@ -1,10 +1,11 @@
 //! This example demonstrates receving event when user does not pick any file
 //! and instead chooses to close the dialog.
 //!
-//! Try to run this example multiple times, choosing files one time and closing
-//! the dialog second time.
+//! When user picks a file it prints the file name and asks for another file.
+//! When user closes the dialog, it exits the app.
 
 use bevy::{log::LogPlugin, prelude::*};
+use bevy_app::AppExit;
 use bevy_file_dialog::prelude::*;
 
 fn main() {
@@ -24,14 +25,23 @@ fn load(mut commands: Commands) {
     commands.dialog().load_file::<PrintFileName>();
 }
 
-fn dialog_canceled(mut ev_canceled: EventReader<DialogFileLoadCanceled<PrintFileName>>) {
+fn dialog_canceled(
+    mut ev_canceled: EventReader<DialogFileLoadCanceled<PrintFileName>>,
+    mut ev_exit: EventWriter<AppExit>,
+) {
     for _ in ev_canceled.read() {
-        eprintln!("Canceled dialog for printing file name");
+        eprintln!("Canceled dialog for printing file name, exiting app");
+        ev_exit.send_default();
     }
 }
 
-fn file_selected(mut ev_loaded: EventReader<DialogFileLoaded<PrintFileName>>) {
+fn file_selected(
+    mut commands: Commands,
+    mut ev_loaded: EventReader<DialogFileLoaded<PrintFileName>>,
+) {
     for ev in ev_loaded.read() {
         eprintln!("Selected {}", ev.file_name);
+        eprintln!("Select another or close the dialog...");
+        commands.dialog().load_file::<PrintFileName>();
     }
 }
